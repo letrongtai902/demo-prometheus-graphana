59bfc61abcbed3555465b0485c8eac8b81238d9e
letrongtai902 <letrong.tai242@gmail.com>
2026-04-01 23:30:48 +0700

    feat: wire up DemoApi with EF Core, Prometheus, endpoints, and services

--- src/DemoApi/Program.cs
+++ src/DemoApi/Program.cs
@@ -1,4 +1,33 @@
+using DemoApi.Data;
+using DemoApi.Endpoints;
+using DemoApi.Services;
+using Microsoft.EntityFrameworkCore;
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=demo.db"));
+
+builder.Services.AddHostedService<OrderProcessorService>();
+builder.Services.AddHostedService<StockReplenishmentService>();
+builder.Services.AddHostedService<SystemMetricsCollector>();
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
-app.MapGet("/", () => "DemoApi running");
+
+// Seed database
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    SeedData.Initialize(context);
+}
+
+app.UseHttpMetrics();
+
+app.MapProductEndpoints();
+app.MapOrderEndpoints();
+app.MapHealthChecks("/health");
+app.MapMetrics();
+
 app.Run();
